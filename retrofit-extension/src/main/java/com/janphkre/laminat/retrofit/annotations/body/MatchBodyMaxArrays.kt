20fca2e88e44f52acbf731d9d49bbe2600ee32fe
Jan Phillip Kretzschmar <janphkre@gmx.de>
2019-10-14 12:09:34 +0200

    Allow Body Anntoations for response body classes too

--- retrofit-extension/src/main/java/com/janphkre/laminat/retrofit/annotations/body/MatchBodyMaxArrays.kt
+++ retrofit-extension/src/main/java/com/janphkre/laminat/retrofit/annotations/body/MatchBodyMaxArrays.kt
@@ -1,6 +1,6 @@
 package com.janphkre.laminat.retrofit.annotations.body
 
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class MatchBodyMaxArrays(
     val values: Array<MatchBodyMaxArray>
--- retrofit-extension/src/main/java/com/janphkre/laminat/retrofit/annotations/body/MatchBodyRegexes.kt
+++ retrofit-extension/src/main/java/com/janphkre/laminat/retrofit/annotations/body/MatchBodyRegexes.kt
@@ -1,6 +1,6 @@
 package com.janphkre.laminat.retrofit.annotations.body
 
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class MatchBodyRegexes(
     val values: Array<MatchRegex>
